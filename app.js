48f4e6f5015d7c6101c10514456fb18988cdd77a
Valentina Bacherer <valentina.bacherer@gmail.com>
2021-05-02 20:04:11 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,84 @@
+"use strict";
+var myName = 'valentina';
+var states = 50;
+// adds two numbers
+var sum = 5 + 4;
+// says hello
+function sayHello() {
+    alert('Hello world!');
+}
+sayHello();
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry' + name + ", you aren't old enough to view this page!");
+    }
+}
+var favVegs = ['carrot', 'brocoli', 'rucula'];
+for (var _i = 0, favVegs_1 = favVegs; _i < favVegs_1.length; _i++) {
+    var veg = favVegs_1[_i];
+    console.log(veg);
+}
+var pet = {
+    name: 'perla',
+    breed: 'labrador',
+};
+console.log(pet.name, pet.breed);
+var friends = [
+    {
+        name: 'Charles',
+        age: 21,
+    },
+    {
+        name: 'Abby',
+        age: 27,
+    },
+    {
+        name: 'James',
+        age: 18,
+    },
+    {
+        name: 'John',
+        age: 17,
+    },
+];
+for (var i = 0; i < friends.length; i++) {
+    checkAge(friends[i].name, friends[i].age);
+}
+function getLength(word) {
+    return word.length;
+}
+var phraseLength = getLength('Hello World');
+function finalSentence(num) {
+    if (num % 2 === 0) {
+        console.log('The world is nice and even');
+    }
+    else {
+        console.log('The world is an odd place!');
+    }
+}
+finalSentence(phraseLength);
+var moreFriends = [
+    {
+        name: 'Charl',
+        age: 31,
+    },
+    {
+        name: 'Anny',
+        age: 20,
+    },
+    {
+        name: 'James',
+        age: 18,
+    },
+    {
+        name: 'John',
+        age: 17,
+    },
+    {
+        name: 'Joel',
+        age: 27,
+    },
+];
+for (var i = 0; i < moreFriends.length; i++) {
+    checkAge(moreFriends[i].name, moreFriends[i].age);
+}
---
+++ app.ts
@@ -0,0 +1,105 @@
+const myName: string = 'valentina';
+const states: number = 50;
+// adds two numbers
+const sum: number = 5 + 4;
+
+// says hello
+function sayHello() {
+  alert('Hello world!');
+}
+
+sayHello();
+
+function checkAge(name: string, age: number) {
+  if (age < 21) {
+    alert('Sorry' + name + ", you aren't old enough to view this page!");
+  }
+}
+
+const favVegs: Array<string> = ['carrot', 'brocoli', 'rucula'];
+
+for (let veg of favVegs) {
+  console.log(veg);
+}
+interface IPet {
+  name: string;
+  breed: string;
+}
+
+const pet: IPet = {
+  name: 'perla',
+  breed: 'labrador',
+};
+
+console.log(pet.name, pet.breed);
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const friends: Array<IPerson> = [
+  {
+    name: 'Charles',
+    age: 21,
+  },
+  {
+    name: 'Abby',
+    age: 27,
+  },
+  {
+    name: 'James',
+    age: 18,
+  },
+  {
+    name: 'John',
+    age: 17,
+  },
+];
+
+for (let i = 0; i < friends.length; i++) {
+  checkAge(friends[i].name, friends[i].age);
+}
+
+function getLength(word: string): number {
+  return word.length;
+}
+
+const phraseLength = getLength('Hello World');
+
+function finalSentence(num: number) {
+  if (num % 2 === 0) {
+    console.log('The world is nice and even');
+  } else {
+    console.log('The world is an odd place!');
+  }
+}
+
+finalSentence(phraseLength);
+
+const moreFriends = [
+  {
+    name: 'Charl',
+    age: 31,
+  },
+  {
+    name: 'Anny',
+    age: 20,
+  },
+  {
+    name: 'James',
+    age: 18,
+  },
+  {
+    name: 'John',
+    age: 17,
+  },
+  {
+    name: 'Joel',
+    age: 27,
+  },
+];
+
+for (let i = 0; i < moreFriends.length; i++) {
+  checkAge(moreFriends[i].name, moreFriends[i].age);
+}
